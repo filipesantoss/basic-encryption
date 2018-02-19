b5cdcfef14cfc00f05203c441be5dfa69e6538e0
filipesantoss <filipe.santos@academiadecodigo.org>
2018-02-19 18:41:41 +0000

    added documentation to util package

--- src/main/java/filipesantoss/crypto/util/Constants.java
+++ src/main/java/filipesantoss/crypto/util/Constants.java
@@ -2,9 +2,39 @@ package filipesantoss.crypto.util;
 
 public class Constants {
 
+    /**
+     * The server host name.
+     *
+     * @see java.net.Socket#Socket(String, int)
+     */
     public static final String HOST = "localhost";
+
+    /**
+     * The server port number.
+     *
+     * @see java.net.Socket#Socket(String, int)
+     * @see java.net.ServerSocket#ServerSocket(int)
+     */
     public static final int PORT = 8880;
+
+    /**
+     * The algorithm to be used when generating a KeyPair.
+     *
+     * @see java.security.KeyPairGenerator#getInstance(String)
+     */
     public static final String KEYPAIR_ALGORITHM = "RSA";
+
+    /**
+     * The key size to be used when generating a KeyPair.
+     *
+     * @see java.security.KeyPairGenerator#initialize(int)
+     */
     public static final int KEYPAIR_KEY_SIZE = 4096;
+
+    /**
+     * The algorithm to be used when generating the symmetric key.
+     *
+     * @see javax.crypto.KeyGenerator#getInstance(String)
+     */
     public static final String SYMMETRIC_KEY_ALGORITHM = "DES";
 }
--- src/main/java/filipesantoss/crypto/util/Stream.java
+++ src/main/java/filipesantoss/crypto/util/Stream.java
@@ -3,8 +3,20 @@ package filipesantoss.crypto.util;
 import java.io.*;
 import java.net.Socket;
 
+/**
+ * Utilitary class containing methods to read and write objects from and to object streams, as well as closing them.
+ */
 public class Stream {
 
+    /**
+     * Reads an object from the input stream.
+     *
+     * @param input - the input stream.
+     * @param <T>   - the expected type of the read object.
+     * @return the read object.
+     * @see ObjectInputStream#readObject()
+     */
+    @SuppressWarnings("unchecked")
     public static <T> T read(ObjectInputStream input) {
         T object = null;
 
@@ -13,19 +25,22 @@ public class Stream {
 
         } catch (ClassNotFoundException e) {
             System.err.println("Failed to cast object.");
-        }
-
-        catch (EOFException e) {
-            //TODO reaching end of object is an expected behaviour. fix?
-        }
-
-        catch (IOException e) {
+        } catch (EOFException e) {
+            //reaching end of object is an expected behaviour
+        } catch (IOException e) {
             System.err.println("Failed to read object.");
         }
 
         return object;
     }
 
+    /**
+     * Writes an object to the output stream.
+     *
+     * @param output - the output stream.
+     * @param object - the object to write.
+     * @see ObjectOutputStream#writeObject(Object)
+     */
     public static void write(ObjectOutputStream output, Object object) {
         if (object == null) {
             return;
@@ -40,9 +55,15 @@ public class Stream {
         }
     }
 
-    public static void close(Closeable socket) {
+    /**
+     * Closes a stream.
+     *
+     * @param stream - the stream to close.
+     * @see Closeable#close()
+     */
+    public static void close(Closeable stream) {
         try {
-            socket.close();
+            stream.close();
 
         } catch (IOException e) {
             System.err.println("Failed to close socket.");
